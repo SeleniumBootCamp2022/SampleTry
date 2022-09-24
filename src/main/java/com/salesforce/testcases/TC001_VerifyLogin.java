86f95b3851348e68ae1e5e81beadb401e70b75b4
ragothamang <selbootcamp2022@gmail.com>
2022-09-24 16:57:55 +0530

    Rg making changes

--- src/main/java/com/salesforce/testcases/TC001_VerifyLogin.java
+++ src/main/java/com/salesforce/testcases/TC001_VerifyLogin.java
@@ -14,6 +14,7 @@ public class TC001_VerifyLogin extends ProjectSpecificMethods{
 		authors="Narendran";
 		category ="Smoke";
 		excelFileName="Login";
+		//rg new comment added for resolving conflict
 	}
 	
 	@Test(dataProvider = "fetchData")
