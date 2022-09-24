8fd666d929f827ab665eb6e2250d14d5d1c70c10
ragothamang <selbootcamp2022@gmail.com>
2022-09-24 18:46:36 +0530

    Rago - branch merge

--- src/main/java/com/salesforce/pages/HomePage.java
+++ src/main/java/com/salesforce/pages/HomePage.java
@@ -4,7 +4,7 @@ import com.framework.selenium.api.design.Locators;
 import com.framework.testng.api.base.ProjectSpecificMethods;
 
 public class HomePage extends ProjectSpecificMethods{
-	
+	// rg commit for branch merge
 	public HomePage verifyHomePage() {
 		verifyDisplayed(locateElement(Locators.XPATH, "//span[text()='Home']"));
 		reportStep("Homepage is loaded", "pass");
